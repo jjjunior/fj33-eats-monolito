8c76e9ca62a14f2c4ef8d2ea36c3aa488bc7d800
Jota <jjjunior@gmail.com>
2025-05-01 11:33:04 -0300

    Commit inicial

---
+++ src/main/java/br/com/caelum/eats/model/Avaliacao.java
@@ -0,0 +1,34 @@
+package br.com.caelum.eats.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+@Data
+public class Avaliacao {
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull @PositiveOrZero @Max(5)
+	private Integer nota;
+
+	@Size(max=255)
+	private String comentario;
+
+	@OneToOne(optional=false)
+	private Pedido pedido;
+
+}
